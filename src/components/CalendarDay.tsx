312d56a53c7cab9247d03929e4e4c320fa138a50
ottaviocoelho <ottaviocoelho@gmail.com>
2025-03-07 13:02:43 -0300

    Style and entries

--- src/components/CalendarDay.tsx
+++ src/components/CalendarDay.tsx
@@ -1,17 +1,32 @@
+import { useState } from "react"
 import { CalendarDate } from "../types/Calendar"
 
 type Props = {
-    date: CalendarDate,
-    isFocused: boolean,
-    onClickHandler: () => void
+  date: CalendarDate,
+  isFocused: boolean,
+  onClickHandler: () => void
 }
 
 function CalendarDay({date, isFocused, onClickHandler}: Props) {
-    return (
-        <div className={`day ${isFocused ? 'expanded' : ''}`} onClick={onClickHandler}>
-            {date.day}
-        </div>
-    )
+    
+  const [entries, setEntries] = useState(["Pilates", "Jiu Jitsu", "Academia"])
+  // const [entries, setEntries] = useState([])
+
+  
+
+  return (
+    <div className={`day ${isFocused ? 'expanded' : ''}`} onClick={onClickHandler}>
+      <span>{date.day}</span>
+      {
+        entries.length ? (
+            <div className="entries-list-wrapper">
+              {entries.map(entry => <span className="entry">{entry}</span> )}
+            </div>
+        ) : undefined
+          
+      }
+    </div>
+  )
 }
 
 export default CalendarDay
